a16f8d006fae0c3f798e7c81d0a60d7714a2f802
Shoval Zada <auck10@gmail.com>
2019-07-08 23:43:44 +0300

    Address class Done

--- src/main/java/Address.java
+++ src/main/java/Address.java
@@ -4,28 +4,21 @@ public class Address {
     private String city;
     private Integer postalCode;
 
+    public Address(String streetAddress, String city, Integer postalCode){
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.postalCode = postalCode;
+    }
 
     public String getStreetAddress() {
         return streetAddress;
     }
 
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
-    }
-
     public String getCity() {
         return city;
     }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
     public Integer getPostalCode() {
         return postalCode;
     }
 
-    public void setPostalCode(Integer postalCode) {
-        this.postalCode = postalCode;
-    }
 }
