e2d3b3b3a977b5142157acf2cbaa394adb9269af
Shoval Zada <auck10@gmail.com>
2019-07-09 02:51:54 +0300

    log4j added

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,7 +1,10 @@
 import java.util.HashMap;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.log4j.Logger;
 
 public class Program {
+    final static Logger logger = Logger.getLogger(Program.class);
+
     public static void main(String[] args){
 
         // Input Data
@@ -19,18 +22,22 @@ public class Program {
 
         //Creating a json of the customer data using jackson mapper
         String CustomerJson = ObjectToJson(currentCustomer);
-        System.out.println(CustomerJson);
+
+        //log the creation of the customer and the json
+        LogCustomerCreation(CustomerJson);
 
 
     }
 
+    //create a new customer object
     private static Customer CreateACustomer(String firstName, String lastName, Integer age, String streetAdress, String city,
                                     Integer postalCode, HashMap<String,String> phoneNumbers){
-        //create a new customer object
+
         return new Customer(firstName,lastName,age,streetAdress,city,postalCode,phoneNumbers);
 
     }
 
+    //create a json String of a given object
     private static String ObjectToJson(Object object){
         ObjectMapper mapper = new ObjectMapper();
         try {
@@ -39,5 +46,10 @@ public class Program {
             System.out.println(e.toString());
         }
         return null;
+    }
+
+    private static void LogCustomerCreation(String customerData){
+        logger.info("A new customer has been created.");
+        logger.info("Customer Data: \n" + customerData);
     }
 }
